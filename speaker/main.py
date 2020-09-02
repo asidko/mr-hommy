429f0ef2b4c3015994ea1a4f167053b6944f9528
a.sidko <sidkoo@outlook.com>
2020-09-02 08:40:24 +0300

    Add voice arg

--- speaker/main.py
+++ speaker/main.py
@@ -1,51 +1,58 @@
-from flask import Flask 
+from flask import Flask
 from flask import request
 import os
 import sys
-from urllib.request import quote 
+from urllib.request import quote
 from os import environ
 
 # Fix for Windows to find VLC folder
 if sys.platform.startswith('win'):
-	os.environ['PYTHON_VLC_MODULE_PATH'] = """C:\Program Files\VideoLan"""
-	os.environ['PYTHON_VLC_LIB_PATH'] = """C:\Program Files\VideoLan\VLC\libvlc.dll"""
-	import vlc
-	vlc.Instance()
+  os.environ['PYTHON_VLC_MODULE_PATH'] = """C:\Program Files\VideoLan"""
+  os.environ[
+    'PYTHON_VLC_LIB_PATH'] = """C:\Program Files\VideoLan\VLC\libvlc.dll"""
+  import vlc
+
+  vlc.Instance()
 else:
-	import vlc
+  import vlc
 
-
-app = Flask(__name__) 
+app = Flask(__name__)
 
 RHVOICE_API_URL = environ.get('RHVOICE_API_URL') or 'http://localhost:8000'
+
 
 ###################################
 # REST endpoints
 ###################################
 
 @app.route('/say')
-@app.route('/') 
+@app.route('/')
 def say_as_get_param():
-	text = request.args.get('text') or "привет"
-	play(text)
-	return text
+  text = request.args.get('text') or "привет"
+  voice = request.args.get('voice')
+  play(text, voice)
+  return text
+
 
 @app.route('/text/<the_text>')
 def say_as_path_variable(the_text):
-	text = the_text or "привет"
-	play(text)
-	return text
+  text = the_text or "привет"
+  play(text)
+  return text
+
 
 ###################################
 # Functions
 ###################################
 
-def play(text):
-	text = quote(text)
-	url = RHVOICE_API_URL + "/say?text=" + text
+def play(text, voice):
+  text = quote(text)
+  url = RHVOICE_API_URL + "/say?text=" + text
+  url = url + "&voice=" + voice if voice is not None else url
 
-	player = vlc.MediaPlayer(url)
-	player.play()
-	
-if __name__ == '__main__': 
-	app.run(host='0.0.0.0') 
+  player = vlc.MediaPlayer(url)
+  player.play()
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
