5ea3078e168775ee70c75da56b5bd57660d1636b
a.sidko <sidkoo@outlook.com>
2020-10-14 23:12:27 +0300

    config_provider: fix couchdb connect

--- config_provider/main.py
+++ config_provider/main.py
@@ -1,19 +1,23 @@
 from os import environ
-import couchdb
+import couchdb2
 from flask import Flask, request
 
 # Configuration #############################
 DB_HOST = environ.get('DB_HOST') or 'localhost'
 DB_USER = environ.get('DB_USER') or 'admin'
 DB_PASSWORD = environ.get('DB_PASSWORD') or 'admin'
-DB_URL = f"http://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5984/"
+DB_URL = f"http://{DB_HOST}:5984/"
 
 # Initialization ############################
 app = Flask(__name__)
 
 print('Connecting to: ' + DB_URL)
-server = couchdb.Server(DB_URL)
-db = server['app_configs']
+server = couchdb2.Server(DB_URL, DB_USER, DB_PASSWORD)
+
+if 'app_configs' in server:
+    db = server['app_configs']
+else:
+    db = server.create('app_configs')
 
 
 @app.route('/config/init/<app_name>', methods=['POST'])
